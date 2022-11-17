59e739176dbed3de51c277fddb200286b945ee48
Matthew Schlebusch <schlebus@amazon.com>
2022-11-17 10:58:32 +0000

    Fix strace script to use epoch seconds in name
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

--- strace-run.sh
+++ strace-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timeout 0.5 strace -r -o strace-${EPOCHREALTIME}.file "./target/debug/syscall-sandbox"
+timeout 0.5 strace -r -o strace-"$(date +%s)".file "./target/debug/syscall-sandbox"
